a62ffc49109d39a615a362e12555777e09c71ae3
Umair Saad <121795621+httpsumairsaad1@users.noreply.github.com>
2024-06-01 13:03:16 +0500

    Create Setting_PictureBox.jsx

---
+++ BusinessPage/Setting_Picture.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import "./global.scss";
+import "./index.scss";
+const Root = ({}) => {
+React.useEffect(() => {
+// Initialize the code
+return () => {}
+}, [])
+return (
+<div className={'picture'}>
+    <div className={'picturebox'}>
+    </div>
+    <svg id="0:2289" className={'group--icon-upload'}></svg>
+    <div className={'lorem-ipsum-dolor-sit-amet-consectetur-adipiscing-elit-sed-do-eiusmod-tempor-incididunt-ut-labore-et-dolore-magna-aliqua-ut-enim-ad-minim-veniam-quis-nostrud-exercitation-ullamco-laboris'}>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris.
+    </div>
+</div>
+);
+};
+export default Root
