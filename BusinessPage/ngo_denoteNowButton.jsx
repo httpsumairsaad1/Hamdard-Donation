27210165aaf7db023fa17f7f9c21d392fb1e77f8
Umair Saad <121795621+httpsumairsaad1@users.noreply.github.com>
2024-06-01 12:57:24 +0500

    Create ngo_denoteNowButton.jsx

---
+++ BusinessPage/ngo_denoteNowButton.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import "./global.scss";
+import "./index.scss";
+const Root = ({}) => {
+React.useEffect(() => {
+// Initialize the code
+return () => {}
+}, [])
+return (
+<div className={'button'}>
+    <div className={'details'}>
+        Denote Now
+    </div>
+</div>
+);
+};
+export default Root
