d280cf86fda6e6543a3a836b6af0bc2d49ae49c6
Umair Saad <121795621+httpsumairsaad1@users.noreply.github.com>
2024-06-01 13:07:04 +0500

    Update Profile Button

---
+++ BusinessPage/Setting_UpdateButton.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import "./global.scss";
+import "./index.scss";
+const Root = ({}) => {
+React.useEffect(() => {
+// Initialize the code
+return () => {}
+}, [])
+return (
+<div className={'button'}>
+    <div className={'details'}>
+        Details
+    </div>
+    <svg id="0:1611" className={'chevron-right'}></svg>
+</div>
+);
+};
+export default Root
